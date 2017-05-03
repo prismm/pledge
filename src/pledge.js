f18892f5c1e51d33a3bbcce5fa0c838975e86255
Jason Choe <jasonkmchoe@gmail.com>
2017-05-03 17:11:27 -0400

    passed ch 3

--- src/pledge.js
+++ src/pledge.js
@@ -8,19 +8,51 @@ class $Promise {
     constructor(executor) {
         // if (executor) console.dir(executor);
         this._state = 'pending';
-        this._value = null;
-        if (executor) this._futureVal = executor();
+        this._value = undefined;
+        if (executor) executor(this._internalResolve.bind(this), this._internalReject.bind(this));
+        this._handlerGroups = [];
     }
     _internalResolve(obj) {
         if (this._state === 'pending') {
             this._value = obj;
             this._state = 'fulfilled';
+            while(this._handlerGroups && this._handlerGroups.length){
+              this._handlerGroups.shift().successCb(this._value);
+            }
         }
     }
     _internalReject(obj) {
         if (this._state === 'pending') {
             this._value = obj;
             this._state = 'rejected';
+            console.log('HELLO????????????')
+            while (this._handlerGroups && this._handlerGroups.length){
+                let shifted = this._handlerGroups.shift().errorCb;
+                console.log(shifted);
+                if (shifted) shifted(this._value);
+                else throw new Error();
+            }
+        }
+    }
+
+    //THEN METHOD
+    then(success, error) {
+        //handler groups
+        if (typeof success !== 'function') {
+            success = false;
+        }
+        if (typeof error !== 'function') {
+            error = false;
+        }
+        this._handlerGroups.push({successCb: success, errorCb: error});
+
+        if (this._state === "fulfilled"){
+            this._handlerGroups.shift().successCb(this._value);
+        } else if (this._state === 'rejected'){
+            let shifted = this._handlerGroups.shift().errorCb;
+            console.log('IN THEN');
+            if (shifted) shifted(this._value);
+            // else throw new Error();
         }
     }
 }
--- test/pledge.spec.ch1.js
+++ test/pledge.spec.ch1.js
@@ -227,7 +227,7 @@ describe('Chapter 1: Structure and State', function() {
             // like "cannot read X of undefined". Think carefully; you may have an
             // issue with *context* (the `this` keyword).
 
-            xit('resolves the promise', function() {
+            it('resolves the promise', function() {
                 var promise = new $Promise(function(resolve) {
                     resolve('WinGARdium leviOHsa.');
                 });
@@ -240,7 +240,7 @@ describe('Chapter 1: Structure and State', function() {
             // After all, you worked so hard to make sure `._internalResolve` and
             // `._internalReject` work properly.
 
-            xit('is indistinguishable in behavior from `._internalResolve`', function() {
+             it('is indistinguishable in behavior from `._internalResolve`', function() {
                 var resolver;
                 var promise = new $Promise(function(resolve) {
                     resolve('Use the promise machinery, Luke.');
@@ -261,7 +261,7 @@ describe('Chapter 1: Structure and State', function() {
 
             // Yet again, resolution and rejection are basically the same.
 
-            xit('rejects the promise', function() {
+            it('rejects the promise', function() {
                 var promise = new $Promise(function(resolve, reject) {
                     reject('Stupefy!');
                 });
@@ -269,7 +269,7 @@ describe('Chapter 1: Structure and State', function() {
                 expect(promise._value).toBe('Stupefy!');
             });
 
-            xit('is indistinguishable in behavior from `._internalReject`', function() {
+            it('is indistinguishable in behavior from `._internalReject`', function() {
                 var rejector;
                 var promise = new $Promise(function(resolve, reject) {
                     reject('You must unlearn what you have learned.');
@@ -288,7 +288,7 @@ describe('Chapter 1: Structure and State', function() {
 
         // This part should pass if you did the above correctly. Follow the logic:
 
-        xit('therefore allows the *creator* of a new promise to control its fate, even asynchronously!', function(done) {
+        it('therefore allows the *creator* of a new promise to control its fate, even asynchronously!', function(done) {
 
             var promise3 = new $Promise(function(resolve) {
                 setTimeout(function runsInTheFuture() {
